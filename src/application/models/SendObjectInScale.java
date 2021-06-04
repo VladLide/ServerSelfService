f0f28a8d8b109488a38a8be0758e36467203e3f1
LichHunter <alexander0derevianko@gmail.com>
2021-06-04 14:17:31 +0300

    Cleaning up in SendObjectInScale

--- src/application/models/SendObjectInScale.java
+++ src/application/models/SendObjectInScale.java
@@ -15,108 +15,106 @@ import org.apache.logging.log4j.Logger;
 import java.util.Locale;
 
 public class SendObjectInScale extends Task<Void> {
-    private Logger logger = LogManager.getLogger(SendObjectInScale.class);
-    private PackageSend pack = null;
-    private final int MAX_WORK;
+	private final Logger logger = LogManager.getLogger(SendObjectInScale.class);
+	private final PackageSend pack;
+	private final int MAX_WORK;
 
-    public SendObjectInScale(PackageSend pack) {
-        this.pack = pack;
-        MAX_WORK = pack.getItems().size() * pack.getConnectSend().size();
-    }
+	public SendObjectInScale(PackageSend pack) {
+		this.pack = pack;
+		MAX_WORK = pack.getItems().size() * pack.getConnectSend().size();
+	}
 
-    @Override
-    public Void call() {
-        process();
-        return null;
-    }
+	@Override
+	public Void call() {
+		process();
+		return null;
+	}
 
-    private void process() {
-        String startTransmissionMessage = LogInfo.sendObj[0];
-        String canceledMessage = LogInfo.sendObj[1];
-        MainWindowCtrl.setLog(startTransmissionMessage);
-        updateMessage(startTransmissionMessage);
+	private void process() {
+		String startTransmissionMessage = LogInfo.sendObj[0];
+		String canceledMessage = LogInfo.sendObj[1];
+		MainWindowCtrl.setLog(startTransmissionMessage);
+		updateMessage(startTransmissionMessage);
 
-        ObservableList<ScaleItemMenu> scaleItemMenus = pack.getConnectSend();
-        ObservableList<Object> packItems = pack.getItems();
-        int countSend = 0;
-        int scaleItemMenusSize = scaleItemMenus.size();
+		ObservableList<ScaleItemMenu> scaleItemMenus = pack.getConnectSend();
+		ObservableList<Object> packItems = pack.getItems();
+		int countSend = 0;
+		int scaleItemMenusSize = scaleItemMenus.size();
 
-        try {
-            for (int i = 0; i < scaleItemMenus.size(); i++) {
-                ScaleItemMenu scaleItemMenu = scaleItemMenus.get(i);
+		try {
+			for (int i = 0; i < scaleItemMenus.size(); i++) {
+				ScaleItemMenu scaleItemMenu = scaleItemMenus.get(i);
 
-                String startText = String.format("%s > %s %s: ",
-                        pack.getType(),
-                        scaleItemMenu.getName(),
-                        scaleItemMenu.getId());
+				String startText = String.format("%s > %s %s: ",
+						pack.getType(),
+						scaleItemMenu.getName(),
+						scaleItemMenu.getId());
 
-                for (int j = 0; j < packItems.size(); j++) {
-                    Object packItem = packItems.get(j);
+				for (int j = 0; j < packItems.size(); j++) {
+					Object packItem = packItems.get(j);
 
-                    if (isCancelled()) {
-                        MainWindowCtrl.setLog(canceledMessage);
-                        updateMessage(canceledMessage);
-                        return;
-                    }
+					if (isCancelled()) {
+						MainWindowCtrl.setLog(canceledMessage);
+						updateMessage(canceledMessage);
+						return;
+					}
 
-                    String s = send(packItem, scaleItemMenu.getDB());
+					String s = send(packItem, scaleItemMenu.getDB());
 
-                    if (s.length() > 0)
-                        countSend++;
+					if (s.length() > 0)
+						countSend++;
 
-                    String message = String.format("%s %s all %d/%d",
-                            startText,
-                            s,
-                            (i * scaleItemMenusSize + j + 1),
-                            MAX_WORK);
-                    MainWindowCtrl.setLog(message);
-                    updateMessage(message);
-                    updateProgress((long) i * scaleItemMenusSize + j + 1, MAX_WORK);
+					String message = String.format("%s %s all %d/%d",
+							startText,
+							s,
+							(i * scaleItemMenusSize + j + 1),
+							MAX_WORK);
+					MainWindowCtrl.setLog(message);
+					updateMessage(message);
+					updateProgress((long) i * scaleItemMenusSize + j + 1, MAX_WORK);
 
-                    try {
-                        Thread.sleep(300);
-                    } catch (InterruptedException interrupted) {
-                        if (isCancelled()) {
-                            MainWindowCtrl.setLog(canceledMessage);
-                            updateMessage(canceledMessage);
-                            return;
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
+					Thread.sleep(300);
+				}
+			}
+		} catch (InterruptedException e) {
+			if (isCancelled()) {
+				MainWindowCtrl.setLog(canceledMessage);
+				updateMessage(canceledMessage);
+				return;
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 
-        String successMessage = LogInfo.sendObj[2] + " " + countSend + "/" + MAX_WORK;
-        MainWindowCtrl.setLog(successMessage);
-        updateMessage(successMessage);
-    }
+		String successMessage = LogInfo.sendObj[2] + " " + countSend + "/" + MAX_WORK;
+		MainWindowCtrl.setLog(successMessage);
+		updateMessage(successMessage);
+	}
 
-    private String send(Object obj, MySQL db) {
-        switch (obj.getClass().getSimpleName().toLowerCase(Locale.ROOT)) {
-            case "goods": {
-                Goods goods = (Goods) obj;
-                goods.save(db);
-                return goods.getId() + " - " + goods.getName();
-            }
-            case "templates": {
-                Templates tmp = (Templates) obj;
-                tmp.save(db);
-                return tmp.getId() + " - " + tmp.getName();
-            }
-            case "codes": {
-                Codes tmp = (Codes) obj;
-                tmp.save(db);
+	private String send(Object obj, MySQL db) {
+		switch (obj.getClass().getSimpleName().toLowerCase(Locale.ROOT)) {
+			case "goods": {
+				Goods goods = (Goods) obj;
+				goods.save(db);
+				return goods.getId() + " - " + goods.getName();
+			}
+			case "templates": {
+				Templates tmp = (Templates) obj;
+				tmp.save(db);
+				return tmp.getId() + " - " + tmp.getName();
+			}
+			case "codes": {
+				Codes tmp = (Codes) obj;
+				tmp.save(db);
 
-                return tmp.getId() + " - " + tmp.getName();
-            }
-            case "settings": {
+				return tmp.getId() + " - " + tmp.getName();
+			}
+			case "settings": {
 
-                return "";
-            }
-            default:
-                return "";
-        }
-    }
+				return "";
+			}
+			default:
+				return "";
+		}
+	}
 }
