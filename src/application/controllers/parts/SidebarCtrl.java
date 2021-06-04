fbb57f2bf7a5fe1867af1fe265848e98a9efec67
LichHunter <alexander0derevianko@gmail.com>
2021-06-04 13:25:39 +0300

    Added method to update status at the menu

--- src/application/controllers/parts/SidebarCtrl.java
+++ src/application/controllers/parts/SidebarCtrl.java
@@ -1,10 +1,6 @@
 package application.controllers.parts;
 
-import java.io.IOException;
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
-
+import application.ScaleStatus;
 import application.controllers.MainCtrl;
 import application.controllers.windows.MainWindowCtrl;
 import application.controllers.windows.ScaleCtrl;
@@ -14,79 +10,84 @@ import application.models.net.mysql.interface_tables.ScaleItemMenu;
 import application.models.net.mysql.tables.Scales;
 import application.models.objectinfo.NodeTree;
 import application.views.languages.uk.parts.SidebarInfo;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableView;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class SidebarCtrl {
 	private ScaleCtrl scaleWindow = null;
 	private AnchorPane sidebar;
 	private NodeTree tempEdit = null;
+	private static SidebarCtrl sidebarCtrl;
 
-    @FXML
-    private ResourceBundle resources = Utils.getResource(Configs.getItemStr("language"),"part","Sidebar");
-    @FXML
-    private URL location = getClass().getResource(Utils.getView("part", "Sidebar"));
-    @FXML
-    private TreeTableView<?> filter;
-    @FXML
-    private AnchorPane menuPane;
-    @FXML
-    private TreeView<NodeTree> menu;
-    @FXML
-    private AnchorPane filterPane;
-    @FXML
-    private Label status;
-    @FXML
-    private MenuItem createCM;
-    @FXML
-    private MenuItem refreshCM;
-    @FXML
-    private MenuItem editCM;
-    @FXML
-    private MenuItem deleteCM;
-    @FXML
-    private ContextMenu sidebarContextMenu;
+	@FXML
+	private ResourceBundle resources = Utils.getResource(Configs.getItemStr("language"), "part", "Sidebar");
+	@FXML
+	private URL location = getClass().getResource(Utils.getView("part", "Sidebar"));
+	@FXML
+	private TreeTableView<?> filter;
+	@FXML
+	private AnchorPane menuPane;
+	@FXML
+	private TreeView<NodeTree> menu;
+	@FXML
+	private AnchorPane filterPane;
+	@FXML
+	private Label status;
+	@FXML
+	private MenuItem createCM;
+	@FXML
+	private MenuItem refreshCM;
+	@FXML
+	private MenuItem editCM;
+	@FXML
+	private MenuItem deleteCM;
+	@FXML
+	private ContextMenu sidebarContextMenu;
 
 	public SidebarCtrl(AnchorPane anchorPane) {
 		try {
-            FXMLLoader loader = new FXMLLoader(location,resources);
-            loader.setController(this);
-            sidebar = MainCtrl.loadAnchorPane(anchorPane, loader);
-        } catch (IOException e) {
-        	sidebar = null;
-        	MainWindowCtrl.setLog(e.getMessage());
-        }
+			FXMLLoader loader = new FXMLLoader(location, resources);
+			loader.setController(this);
+			sidebar = MainCtrl.loadAnchorPane(anchorPane, loader);
+
+			sidebarCtrl = this;
+		} catch (IOException e) {
+			sidebar = null;
+			MainWindowCtrl.setLog(e.getMessage());
+		}
 	}
+
 	public void loadSidebar(AnchorPane anchorPane) {
-		if(sidebar!=null) {
+		if (sidebar != null) {
 			anchorPane.getChildren().add(sidebar);
-		}else {
-			MainWindowCtrl.setLog(this.getClass().getName()+": error null fxml");
+		} else {
+			MainWindowCtrl.setLog(this.getClass().getName() + ": error null fxml");
 		}
 	}
 
 	public void loadMenu(ObservableList<ScaleItemMenu> scales) {
-		if(menu.getRoot() != null)
+		if (menu.getRoot() != null)
 			menu.getRoot().getChildren().clear();
 
-		TreeItem<NodeTree> root = new TreeItem<>(new NodeTree(SidebarInfo.root,"root",0,"root"));
+		TreeItem<NodeTree> root = new TreeItem<>(new NodeTree(SidebarInfo.root, "root", 0, "root"));
 		root.setExpanded(true);
 		SidebarInfo.menu.forEach(mainNode -> {
-			TreeItem<NodeTree> main = new TreeItem<>(new NodeTree(mainNode[1],mainNode[0],1,mainNode[0],root.getValue()));
+			TreeItem<NodeTree> main = new TreeItem<>(new NodeTree(mainNode[1], mainNode[0], 1, mainNode[0], root.getValue()));
 			main.setExpanded(true);
-			switch(mainNode[0]) {
-				case "scales":{
-					if(!scales.isEmpty()) {
-						scales.forEach(scale->{
-							TreeItem<NodeTree> scaleNode = new TreeItem<NodeTree>(
+			switch (mainNode[0]) {
+				case "scales": {
+					if (!scales.isEmpty()) {
+						scales.forEach(scale -> {
+							TreeItem<NodeTree> scaleNode = new TreeItem<>(
 									new NodeTree(
 											scale.getName() + "-" + scale.getId(),
 											"ScaleInfo",
@@ -95,9 +96,8 @@ public class SidebarCtrl {
 											main.getValue()),
 									scale.getImg()
 							);
-							//scaleNode.setExpanded(true);
-							SidebarInfo.menuScale.forEach((value)->{
-								TreeItem<NodeTree> node = new TreeItem<NodeTree>(
+							SidebarInfo.menuScale.forEach(value -> {
+								TreeItem<NodeTree> node = new TreeItem<>(
 										new NodeTree(
 												value[1],
 												value[0],
@@ -111,9 +111,9 @@ public class SidebarCtrl {
 					}
 					break;
 				}
-				case "editors":{
-					SidebarInfo.menuEditors.forEach((value)->{
-						TreeItem<NodeTree> node = new TreeItem<NodeTree> (new NodeTree(value[1],value[0],2,value[0],main.getValue()));
+				case "editors": {
+					SidebarInfo.menuEditors.forEach(value -> {
+						TreeItem<NodeTree> node = new TreeItem<>(new NodeTree(value[1], value[0], 2, value[0], main.getValue()));
 						main.getChildren().add(node);
 					});
 					break;
@@ -123,121 +123,149 @@ public class SidebarCtrl {
 		});
 		menu.setRoot(root);
 		menu.setShowRoot(false);
-		//menu.setContextMenu(arg0);
+
+		//update status line
+		int numberOfScales = root.getChildren().get(0).getChildren().size();
+		long numberOfOnlineScales = scales
+				.stream()
+				.filter(scale ->
+						ScaleStatus
+								.PRODUCTS_AT_DATABASE_ARE_UP_TO_DATE
+								.equals(scale.getStatus()))
+				.count();
+		long numberOfOfflineScales = scales
+				.stream()
+				.filter(scale ->
+						ScaleStatus
+								.NO_CONNECTION
+								.equals(scale.getStatus()))
+				.count();
+		setStatus(String.format("%d scale(s), %d online, %d offline",
+				numberOfScales,
+				numberOfOnlineScales,
+				numberOfOfflineScales));
 	}
+
 	public void addItemMenu(ScaleItemMenu scale) {
 		TreeItem<NodeTree> main = menu.getRoot().getChildren().get(0);
-		TreeItem<NodeTree> scaleNode = new TreeItem<NodeTree>(
+		TreeItem<NodeTree> scaleNode = new TreeItem<>(
 				new NodeTree(
-						scale.getName()+"-"+scale.getId(),
+						scale.getName() + "-" + scale.getId(),
 						"ScaleInfo",
 						2,
 						scale),
 				scale.getImg());
-		SidebarInfo.menuScale.forEach((value)->{
-			TreeItem<NodeTree> node = new TreeItem<NodeTree> (new NodeTree(value[1],"String",3,value[0]));
+		SidebarInfo.menuScale.forEach(value -> {
+			TreeItem<NodeTree> node = new TreeItem<>(new NodeTree(value[1], "String", 3, value[0]));
 			scaleNode.getChildren().add(node);
 		});
 		main.getChildren().add(scaleNode);
 		menu.refresh();
 	}
+
 	public void updateItemMenu(Scales scale) {
-		tempEdit.setName(scale.getName()+"-"+scale.getId());
-		((ScaleItemMenu)tempEdit.getObject()).setScaleServer(scale);
+		tempEdit.setName(scale.getName() + "-" + scale.getId());
+		((ScaleItemMenu) tempEdit.getObject()).setScaleServer(scale);
 		menu.refresh();
 	}
+
 	public void openItemTree(NodeTree node) {
 		switch (node.getLevel()) {
-		case 1:{
-			//openTableScales();
-			break;
-		}
-		case 2:{
-			//openTableInfoScale();
-			//openTableRedactorDataServer
-			MainWindowCtrl.getContentCtrl().showTableRedactorData(node);
-			break;
-		}
-		case 3:{
-			MainWindowCtrl.getContentCtrl().showTableRedactorData(node);
-			//openTableRedactorDataScales();
-			break;
-		}
-		default:
-			break;
+			case 1: {
+				//openTableScales();
+				break;
+			}
+			case 2: {
+				//openTableInfoScale();
+				//openTableRedactorDataServer
+				MainWindowCtrl.getContentCtrl().showTableRedactorData(node);
+				break;
+			}
+			case 3: {
+				MainWindowCtrl.getContentCtrl().showTableRedactorData(node);
+				//openTableRedactorDataScales();
+				break;
+			}
+			default:
+				break;
 		}
 	}
 
-    @FXML
-    void initialize() {
-        assert filter != null : "fx:id=\"filter\" was not injected: check your FXML file 'Sidebar.fxml'.";
-        assert menuPane != null : "fx:id=\"menuPane\" was not injected: check your FXML file 'Sidebar.fxml'.";
-        assert menu != null : "fx:id=\"menu\" was not injected: check your FXML file 'Sidebar.fxml'.";
-        assert filterPane != null : "fx:id=\"filterPane\" was not injected: check your FXML file 'Sidebar.fxml'.";
-        assert status != null : "fx:id=\"status\" was not injected: check your FXML file 'Sidebar.fxml'.";
-        menu.setOnMouseReleased(event->{
-        	TreeItem<NodeTree> node = menu.getSelectionModel().getSelectedItem();
-        	if(node!=null) {
-        		openItemTree(node.getValue());
-        		String path = node.getValue().getName().toUpperCase();
-	        	node = node.getParent();
-	        	while(node.getValue().getLevel() != 0){
-	        		path = node.getValue().getName().toUpperCase() + " > " + path;
-	                node = node.getParent();
-	            }
-	        	MainWindowCtrl.setURL(path);
-        	}
-        });
-        menu.setOnContextMenuRequested(event->{
-        	NodeTree node = menu.getSelectionModel().getSelectedItem().getValue();
-        	if(node.getType().compareToIgnoreCase("ScaleInfo")!=0) {
-        		createCM.setDisable(node.getType().compareToIgnoreCase("scales")!=0);
-    			editCM.setDisable(true);
-    			deleteCM.setDisable(true);
-        		/*if(node.getType().compareToIgnoreCase("scales")!=0) {
-        			sidebarContextMenu.hide();
-        		}*/
-        	}else {
-    			createCM.setDisable(false);
-    			editCM.setDisable(false);
-    			deleteCM.setDisable(false);
-        	}
-        });
-        createCM.setOnAction(event->{
-        	scaleWindow = new ScaleCtrl(new Scales());
-        	scaleWindow.showStage();
-        });
-        refreshCM.setOnAction(event->{
-        	loadMenu(MainCtrl.refreshScales());
-        	menu.refresh();
-        });
-        editCM.setOnAction(event->{
-        	tempEdit = menu.getSelectionModel().getSelectedItem().getValue();
-        	ScaleItemMenu scaleInfo = (ScaleItemMenu)tempEdit.getObject();
-        	scaleInfo.setSaveToConnect(false);
-        	scaleWindow = new ScaleCtrl(scaleInfo.getScaleServer());
-        	scaleWindow.setScaleInfo(scaleInfo);
-        	scaleWindow.showStage();
-        });
-        deleteCM.setOnAction(event->{
-        	TreeItem<NodeTree> item = menu.getSelectionModel().getSelectedItem();
-        	//item.getChildren().clear();
-        	ScaleItemMenu scaleInfo = (ScaleItemMenu)item.getValue().getObject();
-        	scaleInfo.setSaveToConnect(false);
-        	if(scaleInfo.getScaleServer().getUpdate()>=0){
-        		try {
+	@FXML
+	void initialize() {
+		assert filter != null : "fx:id=\"filter\" was not injected: check your FXML file 'Sidebar.fxml'.";
+		assert menuPane != null : "fx:id=\"menuPane\" was not injected: check your FXML file 'Sidebar.fxml'.";
+		assert menu != null : "fx:id=\"menu\" was not injected: check your FXML file 'Sidebar.fxml'.";
+		assert filterPane != null : "fx:id=\"filterPane\" was not injected: check your FXML file 'Sidebar.fxml'.";
+		assert status != null : "fx:id=\"status\" was not injected: check your FXML file 'Sidebar.fxml'.";
+		menu.setOnMouseReleased(event -> {
+			TreeItem<NodeTree> node = menu.getSelectionModel().getSelectedItem();
+			if (node != null) {
+				openItemTree(node.getValue());
+				String path = node.getValue().getName().toUpperCase();
+				node = node.getParent();
+				while (node.getValue().getLevel() != 0) {
+					path = node.getValue().getName().toUpperCase() + " > " + path;
+					node = node.getParent();
+				}
+				MainWindowCtrl.setURL(path);
+			}
+		});
+		menu.setOnContextMenuRequested(event -> {
+			NodeTree node = menu.getSelectionModel().getSelectedItem().getValue();
+			if (node.getType().compareToIgnoreCase("ScaleInfo") != 0) {
+				createCM.setDisable(node.getType().compareToIgnoreCase("scales") != 0);
+				editCM.setDisable(true);
+				deleteCM.setDisable(true);
+			} else {
+				createCM.setDisable(false);
+				editCM.setDisable(false);
+				deleteCM.setDisable(false);
+			}
+		});
+		createCM.setOnAction(event -> {
+			scaleWindow = new ScaleCtrl(new Scales());
+			scaleWindow.showStage();
+		});
+		refreshCM.setOnAction(event -> {
+			loadMenu(MainCtrl.refreshScales());
+			menu.refresh();
+		});
+		editCM.setOnAction(event -> {
+			tempEdit = menu.getSelectionModel().getSelectedItem().getValue();
+			ScaleItemMenu scaleInfo = (ScaleItemMenu) tempEdit.getObject();
+			scaleInfo.setSaveToConnect(false);
+			scaleWindow = new ScaleCtrl(scaleInfo.getScaleServer());
+			scaleWindow.setScaleInfo(scaleInfo);
+			scaleWindow.showStage();
+		});
+		deleteCM.setOnAction(event -> {
+			TreeItem<NodeTree> item = menu.getSelectionModel().getSelectedItem();
+			ScaleItemMenu scaleInfo = (ScaleItemMenu) item.getValue().getObject();
+			scaleInfo.setSaveToConnect(false);
+			if (scaleInfo.getScaleServer().getUpdate() >= 0) {
+				try {
 					scaleInfo.getDB().getDBConnection().close();
 				} catch (SQLException e) {
-					System.out.println("Delete scale >> DB close: "+e.getMessage());
+					System.out.println("Delete scale >> DB close: " + e.getMessage());
 				}
-        	}
-			MainCtrl.getDB().delete("goods_load", "id_scales = "+scaleInfo.getScaleServer().getId());
-        	scaleInfo.getScaleServer().delete(MainCtrl.getDB());
-        	item.getParent().getChildren().remove(item);
-        	menu.refresh();
-        });
-    }
+			}
+			MainCtrl.getDB().delete("goods_load", "id_scales = " + scaleInfo.getScaleServer().getId());
+			scaleInfo.getScaleServer().delete(MainCtrl.getDB());
+			item.getParent().getChildren().remove(item);
+			menu.refresh();
+		});
+	}
+
 	public TreeView<NodeTree> getMenu() {
 		return menu;
+	}
+
+	public void setStatus(String string) {
+		Platform.runLater(() -> status.setText(string));
+	}
+
+	public static SidebarCtrl getInstance() {
+		return sidebarCtrl;
 	}
 }
--- src/application/models/net/mysql/interface_tables/ScaleItemMenu.java
+++ src/application/models/net/mysql/interface_tables/ScaleItemMenu.java
@@ -1,5 +1,6 @@
 package application.models.net.mysql.interface_tables;
 
+import application.ScaleStatus;
 import application.controllers.MainCtrl;
 import application.controllers.windows.MainWindowCtrl;
 import application.models.net.mysql.MySQL;
@@ -26,19 +27,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class ScaleItemMenu {
 	private int id = 0;
 	private String name = "";
-	private LocalDateTime  date_update = LocalDateTime.now();
+	private LocalDateTime date_update = LocalDateTime.now();
 	private ImageView img = new ImageView();
 	private Scales scaleServer = null;
 	private Scales scale = null;
 	private Timer connect = new Timer();
 	private MySQL db = null;
 	private AtomicBoolean saveToConnect = new AtomicBoolean(true);
+	private ScaleStatus status;
 	private static Logger logger = LogManager.getLogger(ScaleItemMenu.class);
 
 	public ScaleItemMenu() {
 		super();
 		this.name = "Server";
 	}
+
 	public ScaleItemMenu(Scales scale) {
 		super();
 		this.id = scale.getId();
@@ -46,17 +49,17 @@ public class ScaleItemMenu {
 		this.scaleServer = scale;
 		img.setFitWidth(30);
 		img.setFitHeight(15);
-		img.setImage(status(scale.getUpdate(),0));
+		img.setImage(status(scale.getUpdate(), 0));
 		this.date_update = scale.getDate_update();
 		connect();
 	}
 
 	public static ObservableList<ScaleItemMenu> get() {
-    	ResultSet result = MainCtrl.getDB().getSelect(Scales.getSql(0,""));
-    	ObservableList<ScaleItemMenu> row = FXCollections.observableArrayList();
-    	try {
-    		while(result.next()) {
-    			ScaleItemMenu item = new ScaleItemMenu(new Scales(result));
+		ResultSet result = MainCtrl.getDB().getSelect(Scales.getSql(0, ""));
+		ObservableList<ScaleItemMenu> row = FXCollections.observableArrayList();
+		try {
+			while (result.next()) {
+				ScaleItemMenu item = new ScaleItemMenu(new Scales(result));
 				row.add(item);
 			}
 		} catch (NullPointerException | SQLException e) {
@@ -65,85 +68,94 @@ public class ScaleItemMenu {
 
 		return row;
 	}
+
 	private Image status(int statusServer, int statusScale) {
 		Color color = null;
-		scaleServer.setUpdate((statusServer==0&&statusScale==2)?statusScale:statusServer);
-		switch(scaleServer.getUpdate()) {
-			case -3:{
+		scaleServer.setUpdate((statusServer == 0 && statusScale == 2) ? statusScale : statusServer);
+		switch (scaleServer.getUpdate()) {
+			case -3: {
 				color = Color.BLACK; //Не вірно введений адрес вагів
+				this.status = ScaleStatus.INCORRECT_SCALE_ADDRESS;
 				break;
 			}
-			case -2:{
+			case -2: {
 				color = Color.RED;//Не має зв'язку
+				this.status = ScaleStatus.NO_CONNECTION;
 				break;
 			}
-			case -1:{
+			case -1: {
 				color = Color.YELLOW;//звязь была потеряна не успев обновиться
+				this.status = ScaleStatus.CONNECTION_WAS_LOST_WITHOUT_UPDATE;
 				break;
 			}
-			case 0:{
+			case 0: {
 				color = Color.GREEN;//товары на бд актуальны на
+				this.status = ScaleStatus.PRODUCTS_AT_DATABASE_ARE_UP_TO_DATE;
 				break;
 			}
-			case 1:{
+			case 1: {
 				color = Color.GREENYELLOW;//обновляются не отключайте
+				this.status = ScaleStatus.UPDATING_DONT_SHUTDOWN;
 				break;
 			}
-			case 2:{
+			case 2: {
 				color = Color.BLUE;//товаров на весах нет
+				this.status = ScaleStatus.NO_PRODUCTS_AT_SCALES;
 				break;
 			}
-			default:{
+			default: {
 				color = Color.BLACK;
 			}
 		}
-		return (new Rectangle(img.getFitWidth(),img.getFitHeight(),color)).snapshot(null, null);
+		return new Rectangle(img.getFitWidth(), img.getFitHeight(), color).snapshot(null, null);
 	}
+
 	public void connect() {
-		if(!scaleServer.getIp_address().matches("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})")||scaleServer.getIp_address().matches("(localhost)|(127\\.0\\.0\\.1)")||scaleServer.getIp_address().length()<7) {
+		if (!scaleServer.getIp_address().matches("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})") || scaleServer.getIp_address().matches("(localhost)|(127\\.0\\.0\\.1)") || scaleServer.getIp_address().length() < 7) {
 			scaleServer.setUpdate(-3);
-			img.setImage(status(scaleServer.getUpdate(),0));
-		}else {
+			img.setImage(status(scaleServer.getUpdate(), 0));
+		} else {
 			scaleServer.setUpdate(-2);
 		}
-		if(scaleServer.getUpdate()!=-3) {
+		if (scaleServer.getUpdate() != -3) {
 			connect.schedule(new TimerTask() {
 				@Override
 				public void run() {
 					try {
-						if(isConnectionDB()) {
-							if(Goods.getCountGoodsLoad(scaleServer.getId(),MainCtrl.getDB())>0) {
+						if (isConnectionDB()) {
+							if (Goods.getCountGoodsLoad(scaleServer.getId(), MainCtrl.getDB()) > 0) {
 								scaleServer.setUpdate(1);
-							}else {
-								if(Goods.getCountGoods(scaleServer.getId(),getDB())<1){
+							} else {
+								if (Goods.getCountGoods(scaleServer.getId(), getDB()) < 1) {
 									scaleServer.setUpdate(2);
-								}else {
+								} else {
 									scaleServer.setUpdate(0);
 								}
 							}
-						}else {
-							if(Goods.getCountGoodsLoad(scaleServer.getId(),MainCtrl.getDB() )>0) {
+						} else {
+							if (Goods.getCountGoodsLoad(scaleServer.getId(), MainCtrl.getDB()) > 0) {
 								scaleServer.setUpdate(-1);
-							}else {
+							} else {
 								scaleServer.setUpdate(-2);
 							}
 						}
-					}catch (SQLException e) {
+					} catch (SQLException e) {
 						System.out.println("ScaleInfo: error " + e.getMessage());
 						scaleServer.setUpdate(-2);
-					}finally {
-						Platform.runLater(()-> {
+					} finally {
+						Platform.runLater(() -> {
 							img.setImage(status(scaleServer.getUpdate(),/*getScale().getUpdate()*/0));
 						});
-							if(saveToConnect.get())scaleServer.save(MainCtrl.getDB());
+						if (saveToConnect.get()) scaleServer.save(MainCtrl.getDB());
 					}
-					if(!MainWindowCtrl.existInstance())connect.cancel();
+					if (!MainWindowCtrl.existInstance()) connect.cancel();
 				}
 			}, 0, 60000);
 		}
 	}
+
 	public static String getSql() {
-		String sql = "SELECT scales.update, count(goods.id) as cout FROM scales" + 
+		String sql = "SELECT scales.update, count(goods.id) as cout FROM scales" +
 				"	Left join goods on goods.id_scales = scales.id;";
 		return sql;
 	}
@@ -152,32 +164,39 @@ public class ScaleItemMenu {
 		this.id = scale.getId();
 		this.name = scale.getName();
 		this.scaleServer = scale;
-		img.setImage(status(scale.getUpdate(),0));
+		img.setImage(status(scale.getUpdate(), 0));
 		this.date_update = scale.getDate_update();
 		getDB().setConfig(scale.getId());
 		saveToConnect.set(true);
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDate_update() {
 		return date_update.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME).replace("T", " ");
 	}
+
 	public void setDate_update(LocalDateTime date_update) {
 		this.date_update = date_update;
 	}
+
 	public Scales getScale() {
-		if(scale==null) {
+		if (scale == null) {
 			this.scale = Scales.get(scaleServer.getId(), "", db);
 		}
 		return this.scale;
 	}
+
 	public void updataScale() {
 		this.scale = Scales.get(scaleServer.getId(), "", db);
 	}
+
 	public int getId() {
 		return id;
 	}
@@ -185,34 +204,48 @@ public class ScaleItemMenu {
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public MySQL getDB() {
-		if(db==null) {
+		if (db == null) {
 			this.db = new MySQL(scaleServer.getId());
 		}
 		return db;
 	}
-	
+
 	public void setDb(MySQL db) {
 		this.db = db;
 	}
-	
+
 	public boolean existConnectionDB() {
-		return getDB().getDBConnection()!=null;
+		return getDB().getDBConnection() != null;
 	}
+
 	public boolean isConnectionDB() throws SQLException {
-		return (existConnectionDB())?getDB().getDBConnection().isValid(5):false;
+		return (existConnectionDB()) ? getDB().getDBConnection().isValid(5) : false;
 	}
+
 	public ImageView getImg() {
 		return img;
 	}
+
 	public Timer getConnect() {
 		return connect;
 	}
+
 	public Scales getScaleServer() {
 		return scaleServer;
 	}
+
 	public void setSaveToConnect(boolean value) {
 		saveToConnect.set(value);
+	}
+
+	public ScaleStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ScaleStatus status) {
+		this.status = status;
 	}
 
 	@Override
